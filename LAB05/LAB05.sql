a8ec45f8753062c8002dfe4c896ccd22f1e0ba50
unknown <2152972@labinfo.is.escuelaing.edu.co>
2018-11-13 12:10:09 -0500

    LAB05

---
+++ LAB05/LAB05.sql
@@ -0,0 +1,62 @@
+<Course_Catalog>
+    <Departament Code = "SE">
+        <Title> System Engineering </Title>
+        <Chair>
+            <Professor>
+                <First_Name>Oswaldo</First_Name>
+                <Last_Name>Castillo</Last_Name>
+            </Professor>
+        </Chair>
+        <Course Number="CS110" Enrollment="2000">
+            <Title>Modelos Matematicos</Title>
+            <Description>
+                Introduccion a la logica matematica.
+            </Description>
+            <Instructors>
+                <Professor>
+                    <First_Name>Patricia</First_Name>
+                    <Last_Name>Alvarez</Last_Name>
+                </Professor>
+                <Professor>
+                    <First_Name>Wilmer</First_Name>
+                    <Last_Name>Garzon</Last_Name>
+                </Professor>
+            </Instructors>
+        </Course>
+        <Course Number="CS111" Enrollment="20001">
+            <Title>Programacion imperativa basica</Title>
+            <Description>
+                Introduccion a la programacion y al diseño de algoritmos para poder desarrollar problemas.
+            </Description>
+            <Instructors>
+                <Professor>
+                    <First_Name>Patricia</First_Name>
+                    <Last_Name>Castañeda</Last_Name>
+                </Professor>
+                <Professor>
+                    <First_Name>Wilmer</First_Name>
+                    <Last_Name>Garzon</Last_Name>
+                </Professor>
+            </Instructors>
+        </Course>
+        <Course Number="CS112" Enrollment="2002">
+            <Title>Programacion imperativa modular</Title>
+            <Description>
+                Profundizacion en la programacion utilizado diferentes metodos logrando asi una mayor eficacia.
+            </Description>
+            <Instructors>
+                <Professor>
+                    <First_Name>Carlos</First_Name>
+                    <Last_Name>Pinzon</Last_Name>
+                </Professor>
+                <Professor>
+                    <First_Name>Diego</First_Name>
+                    <Last_Name>Satoba</Last_Name>
+                </Professor>
+            </Instructors>
+            <Prerequisites>
+                <Prereq>CS111</Prereq>
+            </Prerequisites>
+        </Course>
+    </Departament>
+</Course_Catalog>
