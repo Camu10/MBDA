bc43e9240bd13c3e7bb14cfe5e09d0d89b5adba3
unknown <2152972@labinfo.is.escuelaing.edu.co>
2018-11-13 17:30:48 -0500

    LAB05

--- LAB05/LAB05.sql
+++ LAB05/LAB05.sql
@@ -1,62 +1,66 @@
-<Course_Catalog>
-    <Departament Code = "SE">
-        <Title> System Engineering </Title>
-        <Chair>
-            <Professor>
-                <First_Name>Oswaldo</First_Name>
-                <Last_Name>Castillo</Last_Name>
-            </Professor>
-        </Chair>
-        <Course Number="CS110" Enrollment="2000">
-            <Title>Modelos Matematicos</Title>
-            <Description>
-                Introduccion a la logica matematica.
-            </Description>
-            <Instructors>
-                <Professor>
-                    <First_Name>Patricia</First_Name>
-                    <Last_Name>Alvarez</Last_Name>
-                </Professor>
-                <Professor>
-                    <First_Name>Wilmer</First_Name>
-                    <Last_Name>Garzon</Last_Name>
-                </Professor>
-            </Instructors>
-        </Course>
-        <Course Number="CS111" Enrollment="20001">
-            <Title>Programacion imperativa basica</Title>
-            <Description>
-                Introduccion a la programacion y al diseño de algoritmos para poder desarrollar problemas.
-            </Description>
-            <Instructors>
-                <Professor>
-                    <First_Name>Patricia</First_Name>
-                    <Last_Name>Castañeda</Last_Name>
-                </Professor>
-                <Professor>
-                    <First_Name>Wilmer</First_Name>
-                    <Last_Name>Garzon</Last_Name>
-                </Professor>
-            </Instructors>
-        </Course>
-        <Course Number="CS112" Enrollment="2002">
-            <Title>Programacion imperativa modular</Title>
-            <Description>
-                Profundizacion en la programacion utilizado diferentes metodos logrando asi una mayor eficacia.
-            </Description>
-            <Instructors>
-                <Professor>
-                    <First_Name>Carlos</First_Name>
-                    <Last_Name>Pinzon</Last_Name>
-                </Professor>
-                <Professor>
-                    <First_Name>Diego</First_Name>
-                    <Last_Name>Satoba</Last_Name>
-                </Professor>
-            </Instructors>
-            <Prerequisites>
-                <Prereq>CS111</Prereq>
-            </Prerequisites>
-        </Course>
-    </Departament>
-</Course_Catalog>
+CREATE TABLE validaciones(
+    numero NUMBER(5) NOT NULL
+    ,correo VARCHAR(30) NOT NULL
+    ,fecha DATE NOT NULL
+    ,justificacion VARCHAR(100)
+    ,estado VARCHAR(1) NOT NULL
+    ,codigo NUMBER(2) NOT NULL
+    ,formacion XMLTYPE
+    );
+CREATE TABLE perfiles(
+    correo VARCHAR(30) NOT NULL
+    ,nombre VARCHAR(50) NOT NULL
+    );
+
+ALTER TABLE validaciones ADD CONSTRAINT PK_validaciones PRIMARY KEY(numero);
+ALTER TABLE perfiles ADD CONSTRAINT PK_perfiles PRIMARY KEY(correo);
+ALTER TABLE validaciones ADD CONSTRAINT FK_validaciones_perfiles FOREIGN KEY(correo) REFERENCES perfiles(correo);
+
+ALTER TABLE validaciones ADD CONSTRAINT CK_Tipo_consecutivo_valida
+    CHECK(numero>=1 AND numero<=99999);
+ALTER TABLE validaciones ADD CONSTRAINT CK_Tipo_estado_validacion 
+    CHECK(estado IN('E','T','A')); 
+ALTER TABLE validaciones ADD CONSTRAINT CK_Tipo_correo_validaciones
+    CHECK(correo NOT LIKE '@%' AND correo NOT LIKE '%@' AND correo NOT LIKE '%@%@%' AND correo NOT LIKE '%@@%'  AND correo LIKE '%@%' AND correo NOT LIKE '%hotmail.com' AND correo NOT LIKE '%gmail.com' AND correo NOT LIKE '%yahoo.com'); 
+ALTER TABLE perfiles ADD CONSTRAINT CK_Tipo_correo_perfiles 
+    CHECK(correo NOT LIKE '@%' AND correo NOT LIKE '%@' AND correo NOT LIKE '%@%@%' AND correo NOT LIKE '%@@%'  AND correo LIKE '%@%' AND correo NOT LIKE '%hotmail.com' AND correo NOT LIKE '%gmail.com' AND correo NOT LIKE '%yahoo.com'); 
+ALTER TABLE perfiles ADD CONSTRAINT CK_Tipo_nombre_perfiles
+   CHECK(NOT REGEXP_LIKE(nombre,'[0-9@"#$%&/()=?¿!¡<>*+_.:;|°]') AND nombre LIKE '% %');
+
+INSERT INTO perfiles VALUES('meddolls0@ow.ly','Micky Eddolls');
+INSERT INTO perfiles VALUES('bdeamer1@ifeng.com','Micky Deamer');
+INSERT INTO perfiles VALUES('ystrethill2@phoca.cz','Yasmin Strethill');
+
+INSERT INTO validaciones VALUES(1,'meddolls0@ow.ly',TO_DATE('01-12-2018','dd-mm-yyyy'),NULL,'E',11,
+'<?xml version = "1.0" encoding="UTF-8"?>
+    <Validaciones>
+        <Proyectos>
+            <Nombre> Todos por todos </Nombre>
+            <Año> 2010 </Año>
+            <Rol> Inversor </Rol>
+            <Evaluacion>  </Evaluacion>
+        </Proyectos>
+    </Validaciones>'
+    );
+INSERT INTO validaciones VALUES(2,'bdeamer1@ifeng.com',TO_DATE('10-02-2018','dd-mm-yyyy'),NULL,'A',12,
+'<?xml version = "1.0" encoding="UTF-8"?>
+    <Validaciones>
+        <Proyectos>
+            <Nombre> Oculus Rift </Nombre>
+            <Año> 2018 </Año>
+            <Rol> Contribuyente </Rol>
+            <Evaluacion>  </Evaluacion>
+        </Proyectos>
+    </Validaciones>' 
+    );
+INSERT INTO validaciones VALUES(3,'ystrethill2@phoca.cz',TO_DATE('11-12-2018','dd-mm-yyyy'),NULL,'T',13,
+'<?xml version = "1.0" encoding="UTF-8"?>
+    <Validaciones>
+        <Proyectos>
+            <Nombre> Web bunkers </Nombre>
+            <Año> 2000 </Año>
+            <Rol> Director </Rol>
+            <Evaluacion>  </Evaluacion>
+        </Proyectos>
+    </Validaciones>'
+    );
