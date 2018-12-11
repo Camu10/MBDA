4bbc76aeb413bc91c9f2954bca8d8002ac8e0b22
unknown <2152972@labinfo.is.escuelaing.edu.co>
2018-12-11 16:38:40 -0500

    100%

--- FUENTES/RestriccionesProcedimentales+Automatizacion/DisparadoresOK.sql
+++ FUENTES/RestriccionesProcedimentales+Automatizacion/DisparadoresOK.sql
@@ -1,7 +1,5 @@
-INSERT INTO autores( nombre ) VALUES('Eiichiro Oda');
 INSERT INTO animes ( nombre , genero ) VALUES ('Fairy Tail','Shonen');
 INSERT INTO personajes ( nombre , nombreanime) VALUES ( 'Lucy', 'Fairy Tail');
-INSERT INTO ropa ( categoria , anime) VALUES ( 'Pantalon', 6);
 INSERT INTO figuras ( peranime, tama_o) VALUES ( 'Natzu','Grande');
 INSERT INTO proveedores ( nombre ) VALUES ('Animepro');
 
